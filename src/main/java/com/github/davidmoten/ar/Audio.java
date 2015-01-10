e743748e2c5058b115cdb545d53a14adfc3d5bf5
Dave Moten <davidmoten@gmail.com>
2015-01-10 23:08:51 +1100

    add tests and audio reading code

--- src/main/java/com/github/davidmoten/ar/Audio.java
+++ src/main/java/com/github/davidmoten/ar/Audio.java
@@ -9,6 +9,7 @@ import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.DataLine;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.SourceDataLine;
+import javax.sound.sampled.UnsupportedAudioFileException;
 
 public class Audio {
 
@@ -127,6 +128,68 @@ public class Audio {
 		float h = (T / n);
 		System.out.println("h = " + h
 				+ " (length of each time interval in seconds)");
+	}
+
+	public static void read(InputStream is) {
+
+		// Load the Audio Input Stream from the file
+		AudioInputStream audioInputStream = null;
+		try {
+			audioInputStream = AudioSystem.getAudioInputStream(is);
+		} catch (UnsupportedAudioFileException e) {
+			throw new RuntimeException(e);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+
+		// Get Audio Format information
+		AudioFormat audioFormat = audioInputStream.getFormat();
+
+		// Write the sound to an array of bytes
+		int nBytesRead = 0;
+		byte[] abData = new byte[8192];
+		while (nBytesRead != -1) {
+			try {
+				nBytesRead = audioInputStream.read(abData, 0, abData.length);
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+			if (nBytesRead > 0 && nBytesRead == abData.length) {
+
+				// Determine the original Endian encoding format
+				boolean isBigEndian = audioFormat.isBigEndian();
+
+				int n = nBytesRead / 2;
+
+				// this array is the value of the signal at time i*h
+				Complex x[] = new Complex[n];
+
+				// convert each pair of byte values from the byte array to an
+				// Endian
+				// value
+				for (int i = 0; i < n * 2; i += 2) {
+					int b1 = abData[i];
+					int b2 = abData[i + 1];
+					if (b1 < 0)
+						b1 += 0x100;
+					if (b2 < 0)
+						b2 += 0x100;
+
+					int value;
+
+					// Store the data based on the original Endian encoding
+					// format
+					if (!isBigEndian)
+						value = (b1 << 8) + b2;
+					else
+						value = b1 + (b2 << 8);
+					x[i / 2] = new Complex(value, 0);
+				}
+				FFT.fft(x);
+			}
+
+		}
+
 	}
 
 }
--- src/test/java/com/github/davidmoten/ar/AudioTest.java
+++ src/test/java/com/github/davidmoten/ar/AudioTest.java
@@ -3,7 +3,7 @@ package com.github.davidmoten.ar;
 public class AudioTest {
 
 	public static void main(String[] args) {
-		Audio.play(AudioTest.class.getResourceAsStream("/alphabet.wav"));
+		Audio.read(AudioTest.class.getResourceAsStream("/alphabet.wav"));
 	}
 
 }
