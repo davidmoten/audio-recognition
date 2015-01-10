798e3e485ece18820de2c33e82ea8a034d848edc
Dave Moten <davidmoten@gmail.com>
2015-01-11 09:18:54 +1100

    fix

--- src/main/java/com/github/davidmoten/ar/Complex.java
+++ src/main/java/com/github/davidmoten/ar/Complex.java
@@ -150,28 +150,9 @@ public class Complex {
 		int i = 0;
 		for (Number number : x) {
 			result[i] = new Complex(number.doubleValue(), 0);
+			i++;
 		}
 		return result;
-	}
-
-	// sample client for testing
-	public static void main(String[] args) {
-		Complex a = new Complex(5.0, 6.0);
-		Complex b = new Complex(-3.0, 4.0);
-
-		System.out.println("a            = " + a);
-		System.out.println("b            = " + b);
-		System.out.println("Re(a)        = " + a.re());
-		System.out.println("Im(a)        = " + a.im());
-		System.out.println("b + a        = " + b.plus(a));
-		System.out.println("a - b        = " + a.minus(b));
-		System.out.println("a * b        = " + a.times(b));
-		System.out.println("b * a        = " + b.times(a));
-		System.out.println("a / b        = " + a.divides(b));
-		System.out.println("(a / b) * b  = " + a.divides(b).times(b));
-		System.out.println("conj(a)      = " + a.conjugate());
-		System.out.println("|a|          = " + a.abs());
-		System.out.println("tan(a)       = " + a.tan());
 	}
 
 }
