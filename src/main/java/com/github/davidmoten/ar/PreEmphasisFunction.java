2fb6ff361f41bc627e08088be2c9527c9214bfa7
Dave Moten <davidmoten@gmail.com>
2015-01-14 15:57:04 +1100

    add PreEmphasisFunction

---
+++ src/main/java/com/github/davidmoten/ar/PreEmphasisFunction.java
@@ -0,0 +1,44 @@
+package com.github.davidmoten.ar;
+
+import rx.functions.Func1;
+
+/**
+ * <p>
+ * This function emphasizes higher frequencies in an audio signal represented by
+ * a an array of double. This has the effect of sharpening speech in particular.
+ * </p>
+ * 
+ * <p>
+ * Audio samples <a href=
+ * "http://mirlab.org/jang/books/audiosignalprocessing/speechFeatureMfcc.asp?title=12-2%20MFCC"
+ * >here</a>.
+ * </p>
+ *
+ */
+public class PreEmphasisFunction implements Func1<double[], double[]> {
+
+	private final double proportion;
+
+	public PreEmphasisFunction(double proportion) {
+		this.proportion = proportion;
+	}
+
+	public PreEmphasisFunction() {
+		this(0.95);
+	}
+
+	@Override
+	public double[] call(double[] x) {
+		if (x.length == 0)
+			return new double[0];
+		else {
+			double[] y = new double[x.length];
+			y[0] = x[0];
+			for (int i = 1; i < x.length; i++) {
+				y[i] = x[i] - proportion * x[i - 1];
+			}
+			return y;
+		}
+	}
+
+}
